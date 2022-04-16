b88125f267a5ea7ccbd69b68205ca0c4204991a4
Supratik Chatterjee <supratikdevm96@gmail.com>
2022-04-16 11:44:57 +0530

    Fixes for PyPI

--- setup.py
+++ setup.py
@@ -1,24 +1,31 @@
 from setuptools import setup, Extension, find_packages
 
+
+
+with open('README.md') as f:
+	extd_desc = f.read()
+
 requirements_noversion = [
 'requests'
 ]
 setup(
 	# Meta information
 	name				= 'serp_bot',
-	version				= '0.0.1',
+	version				= '0.1.0',
 	author				= 'Supratik Chatterjee',
-	author_email		= 'chatterjee.supratik@tcs.com',
+	author_email		= 'supratikdevm96@gmail.com',
 	# license			= '2-clause BSD',
-	url					= 'https://github.com/supratikchatterjee16/workbench',
+	url					= 'https://github.com/supratikchatterjee16/serp_bot',
 	description			= 'Search Engine Results Page Bot',
-	keywords			= ['serp bot webscraping scraping search engine package python'],
+	keywords			= ['serp','bot', 'webscraping', 'scraping', 'search engine', 'package', 'python', 'crawler'],
 	install_requires	= requirements_noversion,
 	# build information
 	py_modules			= ['serp_bot'],
 	packages			= find_packages(),
 	package_dir			= {'serp_bot' : 'serp_bot'},
 	include_package_data= True,
+	long_description = extd_desc,
+	long_description_content_type='text/markdown',
 	# package_data		= {'serp_bot' : [
 	# 							'databank/*',
 	# 							'datadump/*',
